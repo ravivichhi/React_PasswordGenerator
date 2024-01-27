0bfbb2f6eaf1ef0b8f1da384b1f242c11da4a5ab
ravi vichhi <vichhiravi18@gmail.com>
2024-01-28 01:06:29 +0530

    add functionality EmailJs email sent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 function App() {
   const [length, setLength] = useState(6)
   const [password, setPassword] = useState(null)
+  const [message, setMessage] = useState(null)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
 
   const passwordRef = useRef(null)
+
+  const form = useRef();
 
   const passwordgenerator = useCallback(() => {
       let pass = '';
@@ -34,9 +38,27 @@ function App() {
     window.navigator.clipboard.writeText(password)
   },[password])
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+        form.current, 
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then((result) => {
+            setMessage('Email sent successfully with your password record!')
+            form.current.reset();
+            
+        }, (error) => {
+            console.log(error.text);
+            console.log("error sending message, try again!")
+        });
+    };
+
   return (
     <>
-      <div className='text-black px-5 py-5 rounded-md text-center justify-center m-5 bg-gray-400 text-black mt-32'>
+      <div className='text-black px-5 py-5 rounded-md text-center justify-center m-5 bg-gray-400 text-black mt-25'>
         <h2 className="text-2xl mb-4"><b>Password Generator</b></h2>
         <div className="flex items-center justify-center mb-4">
           <input
@@ -79,6 +101,46 @@ function App() {
           />
           Characters
         </div>
+      </div>
+        <div className='text-black px-5 py-5 rounded-md text-center justify-center m-5 bg-gray-400 text-black mt-10'>
+        <h2 className="text-2xl mb-4">
+          <b>Email Form</b>
+        </h2>
+          <p style={{ color:'green' }}>{message}</p>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex flex-col items-center justify-center"
+        >
+          <div className="flex items-center justify-center mb-4">
+          <input
+            name="reply_to"
+            type="email"
+            className="border border-gray-600 rounded px-2 py-1 mr-2 w-56 text-black"
+            placeholder="Email"
+            required
+          />
+          <input
+            name="password"
+            type="hidden"
+            className="border border-gray-600 rounded px-2 py-1 mr-2 w-56 text-black"
+            value={password || ''}
+            required
+          />
+          </div>
+          <textarea
+            name="message"
+            className="border border-gray-600 rounded px-2 py-1 mr-2 w-56 text-black mt-2"
+            placeholder="Write message..."
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          >
+            Send Mail
+          </button>
+        </form>
       </div>
     </>
 
